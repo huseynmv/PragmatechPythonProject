2d07887917c67fe5286b2018d1f6806bf032ff21
Huseyn Mammadov <huseynmv@yahoo.com>
2021-10-22 15:01:57 +0400

    Data added from admin panel

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=127, null=True, blank=True)
+    text = models.TextField(help_text='Bura esas melumati daxil edin...')
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=63, verbose_name='Muellif', )
+
+    def __str__(self):
+        return f'Blog {self.title}'
