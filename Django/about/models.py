ff6daa10e8a19ffb9d0418e5d9c1f4d7b9f126f1
Huseyn Mammadov <huseynmv@yahoo.com>
2021-11-05 16:34:11 +0400

    about, blog, product app created

--- Django/about/models.py
+++ Django/about/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+
+class About(models.Model):
+    title = models.CharField(max_length=127, null=True, blank=True)
+    text = models.TextField(help_text='Main info..')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- Django/core/urls.py
+++ Django/core/urls.py
@@ -15,13 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import blog
-from index import views
-from about.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog, name='blog'),
-    path('', views.index),
-    path('about/', about, name='about')
 ]
