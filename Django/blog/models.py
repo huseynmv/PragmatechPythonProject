a11ba2e66b6890df89e255a162ce97948961af53
Huseyn Mammadov <huseynmv@yahoo.com>
2021-11-08 16:24:28 +0400

    Blog model added

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=127, null=True, blank=True)
+    blog_subtitle = models.CharField(max_length=127,null=True,blank=True)
+    blog_desc = models.CharField(max_length=127,null=True,blank=True)
+    
+    def __str__(self) :
+        return self.blog_title
+    
+    class Meta:
+        verbose_name='blog'
+        verbose_name_plural='blogs'
