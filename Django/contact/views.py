f146a649d2506d30d14996ffe3e8fd789193c1c1
Huseyn Mammadov <huseynmv@yahoo.com>
2021-11-10 18:54:46 +0400

    Django messages to user

--- Django/contact/views.py
+++ Django/contact/views.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.shortcuts import redirect, render
 from . forms import ContactForm
+from django.contrib import messages
 
 
 
@@ -10,5 +11,8 @@ def contact(request):
         form = ContactForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            messages.add_message(request, messages.INFO, 'Successfully registered!')
+            return redirect('/contact')
+        else:
+            messages.add_message(request, messages.INFO, 'Wrong email address! Use @gmail.com')
     return render(request, 'contact.html', {'form': form})
