82aa07c0a8b2096302e915586fd8a32d1472239d
Huseyn Mammadov <huseynmv@yahoo.com>
2021-11-20 02:56:41 +0400

    Docker compose

--- Works/Docker/main.py
+++ Works/Docker/main.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from redis import Redis
+
+
 app = Flask(__name__)
+redis_client = Redis(host='127.0.0.1', port = 5000)
+
 
 @app.route('/')
 def index():
-    return '<h1> Hello, Docker! </h1>'
+    hits = redis_client.incr('hits')
+    return f'<h1> Hello, Docker! <br> {hits} defe muraciet edilib </h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
