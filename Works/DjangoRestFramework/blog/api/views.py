a456bec6cac5c4d259cbd0a872aed4b7cc3c7716
Huseyn Mammadov <huseynmv@yahoo.com>
2022-01-22 03:22:53 +0400

    DRF POST method

--- Works/DjangoRestFramework/blog/api/views.py
+++ Works/DjangoRestFramework/blog/api/views.py
@@ -1,10 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from blog.api.serializers import BlogSerializer
 from blog.models import Blog
 
-@api_view(['GET'])
+@api_view(['GET', 'POST',])
 def blog_list_create(request):
-    blog = Blog.objects.all()
-    serializer = BlogSerializer(blog, many = True)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        blog = Blog.objects.all()
+        serializer = BlogSerializer(blog, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response (status=status.HTTP_400_BAD_REQUEST, message = {'message': 'Bad request message'})
