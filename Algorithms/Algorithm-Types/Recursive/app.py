d047e0eda28754a51012203102f353b14a645d9b
Huseyn Mammadov <huseynmv@yahoo.com>
2022-01-28 16:32:48 +0400

    recursion

--- Algorithms/Algorithm-Types/Recursive/app.py
+++ Algorithms/Algorithm-Types/Recursive/app.py
@@ -1,9 +1,20 @@
-def factorial(x):
-    if x == 1:
-        return x 
-    else :
-        return (x * factorial(x-1))
+# def factorial(x):
+#     if x == 1:
+#         return x 
+#     else :
+#         return (x * factorial(x-1))
     
     
-num = 7
-print("The factorial of", num, "is", factorial(num))   
+# num = 7
+# print("The factorial of", num, "is", factorial(num))   
+
+def recursion(k):
+  if(k > 0):
+    result = k + recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+recursion(6)
